1505ff3dcf013d0f72b5edcb75848f898d66599f
Simon Mitternacht <simon.mitternacht@nrk.no>
2020-04-14 20:59:40 +0200

    Explicitly incude header files in sdist.

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ long_description = \
 
 setup(
     name='freesasa',
-    version= '2.0.5',
+    version= '2.0.5.post1',
     description='Calculate solvent accessible surface areas of proteins',
     long_description=long_description,
     author='Simon Mitternacht',
