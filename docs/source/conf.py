273b8cd358607af1c756f58137948cd7520807aa
Simon Mitternacht <simon.mitternacht@nrk.no>
2020-08-16 13:20:46 +0200

    Up version to 2.1.0b1.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ author = 'Simon Mitternacht'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '2.0.5'
+release = '2.1.0b1'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ long_description = \
 
 setup(
     name='freesasa',
-    version= '2.0.5.post2',
+    version= '2.1.0b1',
     description='Calculate solvent accessible surface areas of proteins',
     long_description=long_description,
     author='Simon Mitternacht',
