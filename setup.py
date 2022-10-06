bdfdc46f6fd6cf51e8e4e610b599b54213e25cf5
Simon Mitternacht <simon.mitternacht@gmail.com>
2022-10-06 19:52:25 +0200

    Add missing install in publish script.

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ long_description = \
 
 setup(
     name='freesasa',
-    version= '2.2.0a5',
+    version= '2.2.0a6',
     description='Calculate solvent accessible surface areas of proteins',
     long_description=long_description,
     author='Simon Mitternacht',
