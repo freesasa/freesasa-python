2fd9c8810c8a1c47c3e7655b68661b00253998b7
Simon Mitternacht <simon.mitternacht@gmail.com>
2022-11-13 11:02:16 +0100

    Exclude the combination python 3.11 and macos from publish workflow.

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ long_description = \
 
 setup(
     name='freesasa',
-    version= '2.2.0b1',
+    version= '2.2.0b2',
     description='Calculate solvent accessible surface areas of proteins',
     long_description=long_description,
     author='Simon Mitternacht',
