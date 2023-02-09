d3d5ad0a1d825d910fba3725075f0d929a3a2624
Simon Mitternacht <simon.mitternacht@gmail.com>
2023-02-09 19:54:01 +0100

    Try using older version of setup-python action.

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ long_description = \
 
 setup(
     name='freesasa',
-    version= '2.2.0rc3',
+    version= '2.2.0rc4',
     description='Calculate solvent accessible surface areas of proteins',
     long_description=long_description,
     author='Simon Mitternacht',
