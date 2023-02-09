58d122d2e874494b8a0b5b5b53e81656a454d7a7
Simon Mitternacht <simon.mitternacht@gmail.com>
2023-02-09 20:06:49 +0100

    Try skipping premature install step in publis workflow.

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ long_description = \
 
 setup(
     name='freesasa',
-    version= '2.2.0rc4',
+    version= '2.2.0rc5',
     description='Calculate solvent accessible surface areas of proteins',
     long_description=long_description,
     author='Simon Mitternacht',
