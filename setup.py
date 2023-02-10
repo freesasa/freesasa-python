1e5a25590c689be6edec570bc97ff08d4d93c7b3
Simon Mitternacht <simon.mitternacht@gmail.com>
2023-02-10 07:58:46 +0100

    Add Ubuntu to matrix in publish workflow.

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ long_description = \
 
 setup(
     name='freesasa',
-    version= '2.2.0',
+    version= '2.2.0-post1',
     description='Calculate solvent accessible surface areas of proteins',
     long_description=long_description,
     author='Simon Mitternacht',
