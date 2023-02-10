b46a58fc18af8280b885edf34de913bfac82343b
Simon Mitternacht <simon.mitternacht@gmail.com>
2023-02-10 08:08:29 +0100

    Build egg for Linux #33.

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ long_description = \
 
 setup(
     name='freesasa',
-    version= '2.2.0-post1',
+    version= '2.2.0.post2',
     description='Calculate solvent accessible surface areas of proteins',
     long_description=long_description,
     author='Simon Mitternacht',
